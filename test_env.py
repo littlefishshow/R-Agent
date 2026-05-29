0bb11028091d62b5c337de1a1ffa2a1223c4f629
littlefishshow <1790326323@qq.com>
2026-05-29 11:50:36 +0800

    init

--- test_env.py
+++
@@ -1,6 +0,0 @@
-import os
-from core.config import get_model, get_api_key, get_client_type
-
-print("Client Type:", get_client_type())
-print("Model:", get_model())
-print("API Key loaded:", bool(get_api_key()))
